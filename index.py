2eae23f475c1bb82a41690ffb17fe71006f5c17b
Jonibek <chartakiy@gmail.com>
2025-10-30 23:36:24 +0500

    initial commit

---
+++ index.py
@@ -0,0 +1,8 @@
+# hw:
+# department nomi jadval bor, uning title, id columnlari bor
+# country jadvali bor, uning name, country_type bor
+# employees jadvali bor, uning name, last_name, country, department, salary, email, phone numberi bor 
+# 3 jadval uchun CRUD - create, read, update, delete
+# managerda xodimlarni country va departmenti orqali search qilish imkoniyati
+
+print("hello, world!")
