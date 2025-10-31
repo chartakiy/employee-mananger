6706e7f375285309d16c8da4b02b101ebc163237
Jonibek <chartakiy@gmail.com>
2025-10-31 10:09:14 +0500

    view department done

--- index.py
+++ index.py
@@ -5,4 +5,41 @@
 # 3 jadval uchun CRUD - create, read, update, delete
 # managerda xodimlarni country va departmenti orqali search qilish imkoniyati
 
-print("hello, world!")
+import psycopg2
+
+class DataConnect:
+  def __init__(self):
+    self.connection = psycopg2.connect(
+      dbname = 'n71__23_10',
+      user = 'johnibek',
+      password = '123john',
+      host = 'localhost',
+      port = 5433
+    )
+    self.cursor = self.connection.cursor()
+    print("successfully connected to the database")
+  
+  def view_department(self):
+    query = "SELECT department_id, department_name FROM department"
+    self.cursor.execute(query)
+    rows = self.cursor.fetchall()
+    if not rows:
+      print("no department can be found")
+    else:
+      print("\n========= DEPARTMENTS =========")
+
+
+      for department_id, department_name in rows:
+        print(f"Department ID: {department_id}, Department Name: {department_name}")
+
+    return rows
+
+
+def main():
+  db = DataConnect()
+
+  db.view_department()
+
+
+if __name__ == "__main__":
+  main()
